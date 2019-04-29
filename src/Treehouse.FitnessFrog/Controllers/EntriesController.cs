38a166c92693fd0699c1346050ff03e7b173d508
Spencer Bell <spencerb3@live.ca>
2019-04-29 16:31:30 -0600

    Updated the controller to support the "Edit Entry" page

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -47,35 +47,29 @@ namespace Treehouse.FitnessFrog.Controllers
                 //ActivityId = 2
             };
 
-            ViewBag.ActSelListItems = new SelectList(Data.Data.Activities, "Id", "Name");
+            SetupActivitieSelectListItems();
 
             return View(entry);
         }
+
+        
 
         [HttpPost]
         public ActionResult Add(Entry entry)//(DateTime ? date, int ? activityId, double ? duration, Entry.IntensityLevel ? intensity, bool ? exclude, string notes)
         {
             //ModelState.AddModelError("", "This is a global message.");
 
-            //If there aren't any "Duration" field validation erros, then make sure that the duration is grater than "0".
-            if (ModelState.IsValidField("Duration") && entry.Duration <= 0)
-            {
-                ModelState.AddModelError("Duration", "The Duration field value must be greater than '0'.");
+            ValidateEntry(entry);
 
-            }
-
-
-           if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _entriesRepository.AddEntry(entry);
 
                 return RedirectToAction("Index");
-
-                
                 //TODO Display the Entries list page
             }
 
-            ViewBag.ActSelListItems = new SelectList(Data.Data.Activities, "Id", "Name");
+            SetupActivitieSelectListItems();
 
             //string date = Request.Form["Date"]; //ActiityID, Duration, Intensity, Exlude, Notes
             //ViewBag.Date = ModelState["Date"].Value.AttemptedValue;
@@ -87,6 +81,7 @@ namespace Treehouse.FitnessFrog.Controllers
 
             return View(entry);
         }
+      
 
         public ActionResult Edit(int? id) // ? means nullable, allows entries to be successfully passed action methods
         {
@@ -95,7 +90,42 @@ namespace Treehouse.FitnessFrog.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            return View();
+            //TODO Get the requested entry from the repo
+            Entry entry = _entriesRepository.GetEntry((int)id);
+
+            //TODO Return a status of "not found" if the entry wasn't found
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+
+            //TODO Populate the activities select list items Viewbag property
+            SetupActivitieSelectListItems();
+
+            //TODO Pass the entry into the view
+            return View(entry);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Entry entry)
+        {
+            //TODO Validate the entry
+            ValidateEntry(entry);
+
+            //TODO if the entry is valid...
+            //1. Use the repo to update the entry
+            //2. Redirect the user to the "Entrie" list page
+            if (ModelState.IsValid)
+            {
+                _entriesRepository.UpdateEntry(entry);
+
+                return RedirectToAction("Index");
+            }
+
+            //TODO Populate the activites select list items ViewBag
+            SetupActivitieSelectListItems();
+
+            return View(entry);
         }
 
         public ActionResult Delete(int? id)
@@ -107,5 +137,23 @@ namespace Treehouse.FitnessFrog.Controllers
 
             return View();
         }
-    }
-}
+
+
+
+        private void ValidateEntry(Entry entry)
+        {
+            //If there aren't any "Duration" field validation erros, then make sure that the duration is grater than "0".
+            if (ModelState.IsValidField("Duration") && entry.Duration <= 0)
+            {
+                ModelState.AddModelError("Duration", "The Duration field value must be greater than '0'.");
+
+            }
+        }
+
+        private void SetupActivitieSelectListItems()
+        {
+            ViewBag.ActSelListItems = new SelectList(Data.Data.Activities, "Id", "Name");
+        }
+
+    } //END class
+} //END namespace
